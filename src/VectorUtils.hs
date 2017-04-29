4d1dc474278cdd90d61fcb674d29487113a574e4
Murasaki Suisho <chessai@users.noreply.github.com>
2017-04-29 03:22:31 -0500

    Create VectorUtils.hs

---
+++ src/VectorUtils.hs
@@ -0,0 +1,21 @@
+module VectorUtils
+  (
+    allEqual
+  , argmax
+  , flipMatrix
+  ) where
+
+import Prelude hiding (all, elem, head, flip, map, maximum, minimum, tail)
+import Data.Matrix
+import Data.Vector hiding (reverse, toList)
+
+allEqual :: (Eq a) => a -> Vector a -> Bool
+allEqual c xs = all (== head xs) (tail xs)
+
+argmax :: (Ord b) => (a -> b) -> Vector a -> a
+argmax f = Data.Vector.foldl1 (\acc x -> if f x > f acc then x else acc)
+
+flipMatrix :: Matrix a -> Matrix a
+flipMatrix m = transpose $ Data.Matrix.fromList r c (reverse $ toList m)
+             where r = nrows m
+                   c = ncols m
